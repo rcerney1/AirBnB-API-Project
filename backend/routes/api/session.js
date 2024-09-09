3fef23ca5bd182d10f3c512931b72478dae6ceec
rcerney <ryancerney1@gmail.com>
2024-09-09 11:02:17 -0700

    added user logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,40 +7,44 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
-router.post(
-    '/',
-    async (req, res, next) => {
-      const { credential, password } = req.body;
+//User Login API Route
+router.post('/', async (req, res, next) => {
+    const { credential, password } = req.body;
   
-      const user = await User.unscoped().findOne({
+    const user = await User.unscoped().findOne({
         where: {
           [Op.or]: {
             username: credential,
             email: credential
           }
         }
-      });
+    });
   
-      if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
         const err = new Error('Login failed');
         err.status = 401;
         err.title = 'Login failed';
         err.errors = { credential: 'The provided credentials were invalid.' };
         return next(err);
-      }
+    }
   
-      const safeUser = {
+    const safeUser = {
         id: user.id,
         email: user.email,
         username: user.username,
-      };
+    };
   
-      await setTokenCookie(res, safeUser);
+    await setTokenCookie(res, safeUser);
   
-      return res.json({
+    return res.json({
         user: safeUser
-      });
-    }
-  );
+    });
+});
+
+//User Logout API route
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({message: 'success'});
+});
 
 module.exports = router;
