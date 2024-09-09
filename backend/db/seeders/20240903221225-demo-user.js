527ea0413854a5593383aa58dc599abf2960b471
rcerney <ryancerney1@gmail.com>
2024-09-09 14:11:37 -0700

    added firstName lastName col to Users

--- backend/db/seeders/20240903221225-demo-user.js
+++ backend/db/seeders/20240903221225-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'John',
+        lastName: 'stevens',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'user',
+        lastName: 'One',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'user',
+        lastName: 'Two',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
