637a4fd750b50ae26af4008d962762b8ce723970
rcerney <ryancerney1@gmail.com>
2024-09-11 15:24:35 -0700

    finished reviews routes. added validations

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,60 +2,11 @@ const express = require('express');
 const { Spot, Review, User, SpotImage, sequelize, ReviewImage } = require('../../db/models')
 const { requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+const { handleValidationErrors, validateReview, validateSpot } = require('../../utils/validation');
 const { process_params } = require('express/lib/router');
 const router = express.Router();
 
-//middleware to validate spots
-const validateSpot = [
-    check('address')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .withMessage('Street address is required'),
-    check('city')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .withMessage('city is required'),
-    check('state')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .withMessage('state is required'),
-    check('country')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .withMessage('country is required'),
-    check('lat')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .isFloat({
-        min: -90,
-        max: 90
-      })
-      .withMessage('Latitude must be within -90 and 90'),
-    check('lng')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .isFloat({
-        min: -180,
-        max: 180
-      })
-      .withMessage('Latitude must be within -180 and 180'),
-    check('name')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .isLength({max: 50})
-      .withMessage("Name must be less than 50 characters"),
-    check('description')
-      .notEmpty()
-      .withMessage('Description is required'),
-    check('price')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      //must be positive
-      .isFloat({min: 0})
-      .withMessage("Price per day must be a positive number"),
-    handleValidationErrors
-]
+
 
 //Get all Spots
 router.get('/', async (req, res)=> {
@@ -352,11 +303,7 @@ router.delete('/:spotId', requireAuth, async (req, res) => {
 })
 
 //Create a Review for a Spot based on the Spot's id
-
-//validateReview MiddleWare // !!!!!!!!!!!!!!!!!!! DONT FORGET TO DO THIS BABY GIRL !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
-const validateReview = [];
-
-router.post('/:spotId/reviews', requireAuth, async (req, res) => {
+router.post('/:spotId/reviews', requireAuth, validateReview, async (req, res) => {
     const { spotId } = req.params;
     const { review, stars } = req.body;
     const userId = req.user.id;
