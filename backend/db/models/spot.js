043223a58b31f8e6dbf84c1f661ba5c0a8092c61
rcerney <ryancerney1@gmail.com>
2024-09-16 12:22:21 -0700

    added authorizations in utils/auth.js

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Spot.belongsTo(models.User, {
         foreignKey: 'ownerId',
-        as: 'owner'
+        as: 'Owner'
       });
       Spot.hasMany(models.Review,{
         foreignKey: 'spotId'
