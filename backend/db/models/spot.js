b802f6e9a736a747004035a07dbbc9d7f1bf0449
rcerney <ryancerney1@gmail.com>
2024-09-16 16:58:46 -0700

    finished get all current users bookings, and create a booking

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -21,6 +21,9 @@ module.exports = (sequelize, DataTypes) => {
       Spot.hasMany(models.SpotImage, {
         foreignKey: 'spotId',
       });
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId',
+      });
     }
   }
   Spot.init({
