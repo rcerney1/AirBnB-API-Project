59483e57ad4628f59467344a5654e3ae0bfe93d5
rcerney <ryancerney1@gmail.com>
2024-09-24 16:17:55 -0700

    ugh

--- backend/db/migrations/20240910052821-create-booking.js
+++ backend/db/migrations/20240910052821-create-booking.js
@@ -1,5 +1,10 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Bookings', {
@@ -45,9 +50,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = 'Bookings'
     await queryInterface.dropTable('Bookings');
   }
 };
