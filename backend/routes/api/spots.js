180c734f0108355bf7ead1b312a043efcf4319d3
rcerney <ryancerney1@gmail.com>
2024-09-24 18:58:48 -0700

    test2

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -57,7 +57,7 @@ router.get('/', validateParameters, async (req, res)=> {
             {
                 model: Review,
                 attributes: [[Sequelize.fn('AVG', Sequelize.col('stars')), 'avgRating']],
-                group: ['Spot.id']
+               
             },
             {
                 model: SpotImage,
@@ -77,13 +77,14 @@ router.get('/', validateParameters, async (req, res)=> {
             'description',
             'price',
             'createdAt',
-            'updatedAt',
+            'updatedAt'
+            
             
             //[sequelize.literal(test), 'test'],
             //[sequelize.literal(avgRatingQuery), 'avgRating'],
             //[sequelize.literal(previewImageQuery), 'previewImage']
         ],
-        // group: ['Spot.id'], // Ensure proper grouping
+        //group: ['Spot.id'], // Ensure proper grouping
         limit: size,
         offset: (page - 1) * size,
     });
